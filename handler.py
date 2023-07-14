7ea3a8965e2e66491da629b1dfb90201ad37d888
Davin Byeon <bdv111@snu.ac.kr>
2023-07-15 04:57:19 +0900

    Update all matched files in target directory (#1)
    * update all files in target dir
    
    * dfs

--- handler.py
+++ handler.py
@@ -24,29 +24,41 @@ GITHUB_CLIENT = Github(GITHUB_ACCESS_TOKEN)
 
 
 def update_waffle_world_image_tag(event, context):
+    def _update_files_in(target_dir_path: str):
+        target_dir = gh_repo.get_contents(target_dir_path)
+
+        for target_file in target_dir:
+            if target_file.type == "dir":
+                _update_files_in(target_file.path)
+            elif target_file.type == "file":
+                decoded_lines = base64.b64decode(target_file.content).decode("utf-8").split("\n")
+
+                lines = []
+                updated = False
+                for line in decoded_lines:
+                    if f"image: {ECR_HOST}/{ecr_repo}:" in line:
+                        old_image_tag = line.split(":")[-1].split(" ")[0].strip()
+                        line = line.replace(f":{old_image_tag}", f":{image_tag}")
+                        updated = True
+
+                    lines.append(line)
+
+                if updated:
+                    gh_repo.update_file(
+                        target_file.path,
+                        f"Automated commit by ECR push ({ecr_repo}:{image_tag})",
+                        "\n".join(lines),
+                        target_file.sha,
+                    )
+
     try:
         ecr_repo = event["detail"]["repository-name"]  # ex> snutt-dev/snutt-ev
         namespace, app = ecr_repo.split("/")
         image_tag = event["detail"]["image-tag"]
 
         gh_repo = GITHUB_CLIENT.get_repo("wafflestudio/waffle-world")
-        target_file = gh_repo.get_contents(f"apps/{namespace}/{app}/{app}.yaml")
-        decoded_lines = base64.b64decode(target_file.content).decode("utf-8").split("\n")
+        _update_files_in(f"apps/{namespace}")
 
-        lines = []
-        for line in decoded_lines:
-            if f"image: {ECR_HOST}/{ecr_repo}" in line:
-                old_image_tag = line.split(":")[-1].split(" ")[0].strip()
-                line = line.replace(f":{old_image_tag}", f":{image_tag}")
-
-            lines.append(line)
-
-        gh_repo.update_file(
-            target_file.path,
-            f"Automated commit by ECR push ({ecr_repo}:{image_tag})",
-            "\n".join(lines),
-            target_file.sha,
-        )
         return 'SUCCESS'
     except:
         LOG.exception("Lambda function failed:")
